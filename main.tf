6177b5e9f77997284b1bfe9632e4646f85ae6e4c
= <c0baktr@gmail.com>
2025-04-07 16:48:36 -0600

    adding finops tags

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "demo-subnet"
     Environment = "Dev"
