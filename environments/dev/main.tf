9f901bf7a3962dd9f360486bf5956a5c6cc3d684
= <c0baktr@gmail.com>
2025-04-08 03:12:15 -0600

    swapping to modules and environments

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,12 +22,24 @@ data "aws_subnets" "dev" {
     values = ["dev"]
   }
 }
+
+
+module "vpc" {
+  source      = "../../modules/vpc"
+  vpc_name    = "eks-dev-vpc"
+  vpc_cidr    = "10.0.0.0/16"
+  azs         = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  environment = "dev"
+}
+
 module "eks" {
   source = "../../modules/eks"
 
   cluster_name         = var.cluster_name
-  vpc_id               = data.aws_vpc.dev.id
-  subnet_ids           = data.aws_subnets.dev.ids
+  vpc_id               = module.vpc.vpc_id
+  subnet_ids           = module.vpc.subnet_ids
   node_group_min_size  = 1
   node_group_max_size  = 3
   node_group_desired   = 2
