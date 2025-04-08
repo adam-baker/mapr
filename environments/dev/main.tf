5c3c84bd904c010e8c20dbb93cff407a0a997ead
= <c0baktr@gmail.com>
2025-04-08 02:08:07 -0600

    swapping to modules and environments

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,3 +10,22 @@ module "cronjob_hello" {
   image      = "busybox"
   args       = ["echo", "Hello from dev!"]
 }
+
+data "aws_vpc" "dev" {
+  filter {
+    name   = "tag:Name"
+    values = ["dev-vpc"]  # Change to match your actual VPC Name tag
+  }
+}
+
+data "aws_subnets" "dev" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.dev.id]
+  }
+
+  filter {
+    name   = "tag:Environment"
+    values = ["dev"]
+  }
+}
--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,11 +2,11 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.0.0"
 
-  cluster_name    = "scheduler-eks"
-  cluster_version = "1.29"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 
-  vpc_id     = var.vpc_id
-  subnet_ids = var.subnet_ids
+  vpc_id     = data.aws_vpc.vpc_id
+  subnet_ids = data.aws_subnets.subnet_ids
 
   enable_irsa = true
   eks_managed_node_groups = {
