c70c28b395cbb30554fcebb6002975e438a3287f
= <c0baktr@gmail.com>
2025-04-08 01:45:45 -0600

    swapping to modules and environments

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-adambaker"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locks"
-    profile        = "terraform-deployer"
-  }
-}
